881d0ffe5cc90e1f5b68fc932e64f197cc9bc60a
Nasreddine Hanafi <nasr.eddine799@gmail.com>
2022-11-21 01:51:46 +0100

    d02

---
+++ cpp_02/ex02/Fixed.cpp
@@ -0,0 +1,172 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhanafi <nhanafi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 00:40:32 by nhanafi           #+#    #+#             */
+/*   Updated: 2022/11/21 01:51:09 by nhanafi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <cmath>
+
+
+Fixed::Fixed()
+{
+	// std::cout << "Default constructor called" << std::endl;
+	value = 0;
+}
+
+Fixed::Fixed(const Fixed &src)
+{
+	// std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed(const float f)
+{
+	// std::cout << "Float constructor called" << std::endl;
+	this->value = int(f * (1 << this->fracBits));
+}
+
+Fixed::Fixed(const int n)
+{
+	// std::cout << "Int constructor called" << std::endl;
+	this->value = n << this->fracBits;
+}
+
+Fixed::~Fixed()
+{
+	// std::cout << "Destructor called" << std::endl;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	value = raw;
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	return value;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return (float) this->value / (1 << this->fracBits);
+}
+
+int		Fixed::toInt( void ) const
+{
+	return (this->value >= 0 ? 1 : -1) * (abs(this->value) >> this->fracBits);
+}
+
+Fixed	&Fixed::operator=( Fixed const & rhs )
+{
+	// std::cout << "Copy assignment operator called"  << std::endl;
+	this->value = rhs.getRawBits();
+	return *this;
+}
+
+std::ostream &operator<<( std::ostream &o, Fixed const &F)
+{
+	o << F.toFloat();
+	return o;
+}
+
+bool 		Fixed::operator>=( Fixed const & rhs) const
+{
+	return (value >= rhs.getRawBits());
+}
+
+bool 		Fixed::operator>( Fixed const & rhs ) const
+{
+	return (value > rhs.getRawBits());
+}
+
+bool 		Fixed::operator<=( Fixed const & rhs ) const
+{
+	return (value <= rhs.getRawBits());
+}
+
+bool 		Fixed::operator<( Fixed const & rhs ) const
+{
+	return (value < rhs.getRawBits());
+}
+
+bool 		Fixed::operator==( Fixed const & rhs ) const
+{
+	return (value == rhs.getRawBits());
+}
+
+Fixed 		Fixed::operator+( Fixed const & rhs ) const
+{
+	Fixed	res;
+
+	res.setRawBits(this->value + rhs.getRawBits());
+	return res;
+}
+
+Fixed 		Fixed::operator-( Fixed const & rhs ) const
+{
+	Fixed	res;
+
+	res.setRawBits(this->value - rhs.getRawBits());
+	return res;
+}
+
+Fixed 		Fixed::operator*( Fixed const & rhs ) const
+{
+	Fixed	res;
+
+	res.setRawBits((this->value * rhs.getRawBits()) >> res.fracBits);
+	return res;
+}
+
+Fixed 		Fixed::operator/( Fixed const & rhs ) const
+{
+	Fixed	res;
+
+	res.setRawBits((this->value << this->fracBits) / rhs.getRawBits());
+	return res;
+}
+
+Fixed		Fixed::operator++(int)
+{
+	Fixed	res(*this);
+
+	this->value++;
+	return res;
+}
+
+Fixed		Fixed::operator--(int)
+{
+	Fixed	res(*this);
+
+	this->value--;
+	return res;
+}
+
+Fixed		Fixed::operator++(void)
+{
+	this->value++;
+	return *this;
+}
+
+Fixed		Fixed::operator--(void)
+{
+	this->value--;
+	return *this;
+}
+
+Fixed		Fixed::max(Fixed const &F1, Fixed const &F2)
+{
+	return F1 > F2 ? F1 : F2;
+}
+
+Fixed		Fixed::min(Fixed const &F1, Fixed const &F2)
+{
+	return F1 < F2 ? F1 : F2;
+}
---
+++ cpp_02/ex02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhanafi <nhanafi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 20:56:50 by nhanafi           #+#    #+#             */
+/*   Updated: 2022/11/21 01:51:33 by nhanafi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return 0;
+}
