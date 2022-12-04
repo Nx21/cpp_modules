c736159cc27ac569fa59d7277eb6b54cec350ea4
Nasreddine Hanafi <nasr.eddine799@gmail.com>
2022-12-04 05:11:28 +0100

    d05

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -6,11 +6,29 @@
 /*   By: nhanafi <nhanafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 00:22:24 by nhanafi           #+#    #+#             */
-/*   Updated: 2022/12/04 01:19:52 by nhanafi          ###   ########.fr       */
+/*   Updated: 2022/12/04 03:19:05 by nhanafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Convertor.hpp"
+
+bool	isnumber(std::string str)
+{
+    int i;
+
+    i = 0;
+    if(str.empty())
+        return (0);
+    if (str[i] == '-' || str[i] == '+')
+        i++;
+    while(str[i])
+    {
+        if(!isdigit(str[i]))
+            return (0);
+        i++;
+    }
+    return (1);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -20,7 +38,8 @@ int main(int argc, char const *argv[])
         return 1;
     }
     std::string arg(argv[1]);
-    if (arg.size() == 1 && !std::isdigit(arg[0])
-        std::cout << "";
+    if (arg.size() == 1 && !std::isdigit(arg[0]))
+        std::cout << "ojkm";
+    else if ()
         
 }
